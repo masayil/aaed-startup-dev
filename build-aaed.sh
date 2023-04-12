e7ff067155e587e6086f09eae20a7bd4d65c7dae
Linghao <857453455@qq.com>
2023-04-12 16:54:03 +0800

    Update build-aaed.sh

--- build-aaed.sh
+++ build-aaed.sh
@@ -3,12 +3,12 @@
 if [ ! -d "aaed" ]; then
     echo "==没有找到旧的aaed仓库=="
     echo "===克隆新的aaed仓库==="
-    git clone -b dev https://github.com/Ankr-Shanghai/aaed.git
+    git clone -b dev --depth 1 https://github.com/Ankr-Shanghai/aaed.git
 else
     echo "==删除旧的aaed仓库=="
     rm -rf aaed
     echo "===克隆新的aaed仓库==="
-    git clone -b dev https://github.com/Ankr-Shanghai/aaed.git
+    git clone -b dev --depth 1 https://github.com/Ankr-Shanghai/aaed.git
 fi
 
 cd aaed && make geth
